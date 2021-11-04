99681b6e08b7cd4147137a3b13440c676e5e776e
mrq-quintana <mrq.quintana@gmail.com>
2021-11-04 10:54:23 -0300

    Desafio manejo de archivos

--- backend/files/app.js
+++ backend/files/app.js
@@ -3,28 +3,28 @@ const Contenedor = require('../index');
 const contenedor = new Contenedor();
 
 
-contenedor.crearProductList(
-                            {title:"Lupanocho" , 
+contenedor.save(
+                            {title:"Lupu" , 
                             price:"1500" , 
                             image:"Ruta Imagen2" , 
-                            id:"4"}
+                            id:"3"}
 
                             ).then(result=>{
                             console.log(result.message);
                             })
 
-contenedor.getById('4').then(result=>{
-
+contenedor.getById('2').then(result=>{
     console.log(result.product)
+    console.log(result.message);
 })
 
 contenedor.getAll().then(result=>{
-
+    console.log(result.message);
     console.log(result.product)
 })
 
 
 contenedor.deleteById('3').then(result=>{
-
+    console.log(result.message);
     console.log(result.product)
 })
--- backend/index.js
+++ backend/index.js
@@ -1,95 +1,99 @@
-const fs = require('fs');
+const fs = require("fs");
 
-class Contenedor{
-    
-            async crearProductList(product){
-                try {
-                    let info = await fs.promises.readFile('./products.txt','utf-8')
-                    let infoJson = JSON.parse(info);
-                    if (infoJson.some(i=>i.title===product.title)){
-                        return{status:"error", message:"El producto ya existe"}
-                    }else{
-                        
-                        let productos = {
-                            id:product.id,
-                            title: product.title,
-                            price: product.price,
-                            image: product.image,
-                    }
+class Contenedor {
+  async save(product) {
+    try {
+      let info = await fs.promises.readFile("./products.txt", "utf-8");
+      let infoJson = JSON.parse(info);
+      if (infoJson.some((i) => i.title === product.title)) {
+        return { status: "error", message: "El producto ya existe" };
+      } else {
+        let productos = {
+          id: product.id,
+          title: product.title,
+          price: product.price,
+          image: product.image,
+        };
 
-                    infoJson.push(productos);
+        infoJson.push(productos);
 
-                    try {
-                        await fs.promises.writeFile('./products.txt', JSON.stringify(infoJson, null,5));
-                        return {status:"sucess",message:"Producto agregado"}
-                    } catch(error) {
-                        return {status:"error", message:"No se pudo agregar Producto" + error}
-                    }
-                }
-                } catch (error) {
-                    let productos = {
-                        id:product.id,
-                        title: product.title,
-                        price: product.price,
-                        image: product.image,
-                    }
-                    try{
-                        await fs.promises.writeFile('./products.txt', JSON.stringify([productos]),null,2);
-                        return {status:"sucess",message:"Producto agregado"}
-                    }catch(error){
-                        return{status:"error", message:"No se pudo agregar Producto" + error}
-                    }
-                }
-            }
-    
-    // save(objet){}
-    async getById(id){
         try {
-            let info = await fs.promises.readFile('./products.txt','utf-8')
-            let infoJson = JSON.parse(info);
-            let infoId = infoJson.find(i=>i.id===id)
-
-            if (infoId){
-                return{status:"Succes", product:infoId,  message:"Id encontrado"}
-            }
-        }catch(error){
-            return{status:"error", message:"No se pudo aencontrar" + error}
+          await fs.promises.writeFile("./products.txt",JSON.stringify(infoJson, null, 2));
+          return { status: "sucess", message: "Producto agregado" };
+        } catch (error) {
+          return {status: "error",message: "No se pudo agregar Producto" + error};
         }
+      }
+    } catch (error) {
+      let productos = {
+        id: product.id,
+        title: product.title,
+        price: product.price,
+        image: product.image,
+      };
+      try {
+        await fs.promises.writeFile("./products.txt",JSON.stringify([productos]),null,2);
+        return { status: "sucess", message: "Producto agregado" };
+      } catch (error) {
+        return {status: "error", message: "No se pudo agregar Producto" + error};
+      }
     }
-        async getAll(){
-            try {
-                let info = await fs.promises.readFile('./products.txt','utf-8')
-                let infoJson = JSON.parse(info);
-                let infoId = infoJson.map(function(prod){
-                    return prod;
-                  });
-    
-                if (infoId){
-                    return{status:"Succes", product:infoId,  message:"Id encontrado"}
-                }
-            }catch(error){
-                return{status:"error", message:"No se pudo aencontrar" + error}
-            }
-        }
-            async deleteById(id){
-                try {
-                    let info = await fs.promises.readFile('./products.txt','utf-8')
-                    let infoJson = JSON.parse(info);
-                    let infoId = infoJson.filter(i=>i.id!==id)
-        
-                    if (infoId){
-                        return{status:"Succes", product:infoId,  message:"Id encontrado"}
-                    }
-                }catch(error){
-                    return{status:"error", message:"No se pudo aencontrar" + error}
-                }
-            }
-  
-    
-    // deleteAll(){}
+  }
 
-        
+  
+  async getById(id) {
+    try {
+      let info = await fs.promises.readFile("./products.txt", "utf-8");
+      let infoJson = JSON.parse(info);
+      let infoId = infoJson.find((i) => i.id === id);
+
+      if (infoId) {
+        // return {product: infoId, message: "Id encontrado", };
+        return { status: "Succes", product: infoId, message: "Id encontrado" };
+      } 
     
+    } catch (error) {
+    //   return {message: "No se pudo encontrar id",};
+      return { status: "error", message: "No se pudo aencontrar Id " + error };
+    }
+  }
+  async getAll() {
+    try {
+      let info = await fs.promises.readFile("./products.txt", "utf-8");
+      let infoJson = JSON.parse(info);
+      let infoId = infoJson.map((product)=>product);
+
+      if (infoId) {
+        return { status: "Succes", product: infoId, message: "Estos son todos los productos" };
+      }
+    } catch (error) {
+      return { status: "error", message: "No se pudo aencontrar" + error };
+    }
+  }
+  async deleteById(id) {
+    try {
+      let info = await fs.promises.readFile("./products.txt", "utf-8");
+      let infoJson = JSON.parse(info);
+      let infoId = infoJson.filter((i) => i.id !== id);
+
+      if (infoId) {
+        try {
+            await fs.promises.writeFile("./products.txt",JSON.stringify(infoId, null, 2));
+            return { status: "sucess", message: "Producto agregado" };
+          }catch{
+        return { status: "Succes", product: infoId, message: "El producto se elimino correctamente" };
+    }
+      }
+    } catch (error) {
+      return { status: "error", message: "No se pudo eliminar producto" + error };
+    }
+  }
+
+
+
+
+
+  // deleteAll(){}
 }
 
 module.exports = Contenedor;
